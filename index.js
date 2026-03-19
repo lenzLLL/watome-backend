907ec4d1ef767ff792d257652800c0374136280b
lenz <lenzyounda@gmail.com>
2026-03-19 16:45:11 +0100

    13

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(cookieParser())
 app.use(express.json({limit: '50mb'}))
 app.use("/api/auth",authRoutes)
