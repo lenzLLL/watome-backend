b5d28af616699b15837b8d574233fe4a10ad473b
lenz <lenzyounda@gmail.com>
2026-03-19 17:09:30 +0100

    16

--- index.js
+++ index.js
@@ -57,19 +57,8 @@ const corsOptions = {
   ],
   credentials: true,
 };
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://watome-frontend.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-//   res.header("Access-Control-Allow-Credentials", "true");
 
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-
-//   next();
-// });
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cookieParser())
 app.use(express.json({limit: '50mb'}))
 app.use("/api/auth",authRoutes)
